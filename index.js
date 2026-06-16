d66595a411e8595a1ab7a20d2b88a91dfe13c901
JerryYaj22 <JerryYaj22@gmail.com>
2026-06-16 04:22:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
    CONFIG
 ========================= */
 const SUPABASE_URL = "https://obnpotxehcktfdcseocz.supabase.co";
-const SUPABASE_KEY = "YOUR_KEY_HERE";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ibnBvdHhlaGNrdGZkY3Nlb2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNjU4NTQsImV4cCI6MjA5Njk0MTg1NH0.ewsB-9T3j1V2BlhaDai9OpIAbPWK6NDQpvPf5SRNFfA";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
