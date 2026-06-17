b5b8676898ae3fdd152e5fd3bd4791a3d0a2e7d2
JerryYaj22 <JerryYaj22@gmail.com>
2026-06-17 11:55:33 -0500

    Update success.js

--- success.js
+++ success.js
@@ -4,3 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("trackBtn").href =
     `trackprogress.html?order=${orderId}`;
 });
+const copyBtn = document.getElementById("copyBtn");
+const orderNumber = document.getElementById("orderNumber");
+
+copyBtn.addEventListener("click", async () => {
+    try {
+        await navigator.clipboard.writeText(orderNumber.textContent.trim());
+
+        copyBtn.innerHTML = `
+            <i class="fa-solid fa-check"></i>
+            Copied!
+        `;
+
+        setTimeout(() => {
+            copyBtn.innerHTML = `
+                <i class="far fa-copy"></i>
+                Copy
+            `;
+        }, 2000);
+
+    } catch (err) {
+        console.error("Failed to copy:", err);
+    }
+});
